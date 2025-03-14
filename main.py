394df442d45e6b8b18eeb4a018e8f2e414e65092
RafinEazdan <eazdan.rafin@gmail.com>
2025-03-14 11:46:49 +0600

    cors is updated for better access

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3003"],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],  
+    allow_methods=["*"],  
     allow_headers=["*"],  
 )
 
