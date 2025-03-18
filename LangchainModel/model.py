034164526ab4e22fa6d46664f0164e6004d445a7
Eazdan Mostafa Rafin <eazdan.rafin@gmail.com>
2025-03-19 05:21:01 +0600

    ollama is introduced so no more 500 internal server error

--- LangchainModel/model.py
+++ LangchainModel/model.py
@@ -1,17 +1,16 @@
 import json
-import os
-from langchain.chat_models import init_chat_model
-from langchain.schema import HumanMessage
+from langchain.chat_models import ChatOllama
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+# GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+#
+# if not GROQ_API_KEY:
+#     raise ValueError("GROQ_API_KEY is not set in the environment variables.")
 
-if not GROQ_API_KEY:
-    raise ValueError("GROQ_API_KEY is not set in the environment variables.")
-
-model = init_chat_model("llama3-8b-8192", model_provider="groq")
+# model = init_chat_model("llama3-8b-8192", model_provider="groq")
+model = ChatOllama(model="llama3.2")
 
 
 def review_cv(cv_data):
@@ -26,7 +25,8 @@ def review_cv(cv_data):
     Provide your response in JSON format only, with no additional text:
     {{"rating": <score>,"rating_analysis":"<analysis at most 100 words>", "profession_recommendation": "<profession>"}}
     """
-    response = model.invoke([HumanMessage(content=prompt)]).content
+
+    response = model.invoke(prompt).content
 
     try:
         response_dict = json.loads(response)
@@ -38,7 +38,7 @@ def review_cv(cv_data):
 def compare_cvs(cv_list):
     prompt = f"""
     Compare the CVs and identify the best industry and role for each cv.  Like if a cv is suited for software engineer than also select which stack fronend or backend or fullstack. like a cv is suited businessman than which type of business. Provide a detailed explanation for your choices.
-
+    dont explicitly return the role. make it inside the industry.
     CVs:
     {json.dumps(cv_list, indent=2)}
     In the summary_analysis, use the person's first name (e.g., Alice) instead of indexing the CVs.
@@ -52,7 +52,8 @@ def compare_cvs(cv_list):
     }}
     """
 
-    response = model.invoke([HumanMessage(content=prompt)]).content
+
+    response = model.invoke(prompt).content
     try:
         response_list = json.loads(response)
         return response_list
